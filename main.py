d28dd83c24bd00808b308aad7dbcf2c2ce08aa45
DINOTICK <79216416+DINOTICK-Dev@users.noreply.github.com>
2023-02-07 17:03:40 -0500

    more changes

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import requests
+import os
 
-r = requests.get("https://northstar.tf/client/servers")   # get the server list
+r = requests.get("https://northstar.tf/client/servers")     # get the server list
 
 serverindex = 0
-indexlimit = 20
-if r.status_code == 200:
-  for server in r.json():    # iterate through server list
-    servername = server["name"]
-    playercount = server["playerCount"]
-    playermax = server["maxPlayers"]
-    serverregion = server["region"]
-    servermap = server["map"]
-    print(servername)
-    print("Map: " + servermap)
-    print("Region: " + serverregion)
-    print("Playercount: " + "[" + str(playercount) + "/" + str(playermax) + "]")
-    print("=========================")
+def ServerGet():
+  if r.status_code == 200:
+    for server in r.json():    # iterate through server list
   
-    serverindex += 1
-    if serverindex >= indexlimit:
+      try:
+        serverregion = server["region"]
+      except:
+        serverregion = ["Unknown"]
+        
+      if server["hasPassword"] == True:
+        serverpass = "[Password Protected]"
+      else:
+        serverpass = ""  
+      
+      
+      servername = server["name"]
+      playercount = server["playerCount"]
+      playermax = server["maxPlayers"]
+      servermap = server["map"]
+        
+      print(servername + "     " + serverpass)
+      print("Map: " + servermap)
+      
+      try:
+        print("Region: " + serverregion)
+      except:
+        print("Region: Unknown")
+        
+      print("Playercount: " + "[" + str(playercount) + "/" + str(playermax) + "]")
+      print("=========================")
+      
       break
-else:
-  print("Error gathering information")
+  
+  else:
+    print("Error gathering information")
+
+ServerGet()
