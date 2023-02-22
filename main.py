3383c80c42d4f76301e1f7ff301128102db1f606
DINOTICK <79216416+DINOTICK-Dev@users.noreply.github.com>
2023-02-22 15:13:23 -0500

    Update main.py
    Added a refresh function that refreshes the server browser and player count information, worked on adding a CLI interface for the user to work with

--- main.py
+++ main.py
@@ -1,21 +1,62 @@
 import requests
+import sys
 import os
+import time
 from func.playcnt import *
 from func.servbrow import *
 from func.servcnt import *
-from rich.console import Console
-from rich.table import Table
+from func.playcnt import refresh as playref
+from func.servbrow import refresh as browref
+from func.servcnt import refresh as servref
 from rich import print
-from rich.layout import Layout
 
 """
 Ion Server Browser
 Made by DINOTICK
-Ion Server Browser is a simple server browser made in python for the Titanfall 2 community mod "Northstar" that has some tools for the northstar serverbrowser
-
-How it Works:
-The files inside of the folder "func" are used for obtaining information, such as players, maxplayers, a parsable dictionary (oo lah lah!) and servercount. Then you can import variables from this main.py file here! Easy as pie... Or cake... Or whatever you call it.
 """
 
+def ErrorMSG(sleeptime):
+  time.sleep(sleeptime)
+  sys.exit()
+
 def cls():
+  try:
     os.system('cls' if os.name=='nt' else 'clear')
+  except:
+    print("[red]There was an error wiping the screen, are you on a supported operating system? (Linux, Windows)[/red]")
+    ErrorMSG(2)
+    
+def RefreshAll(): 
+  try:
+    playref()
+  except:
+    print("[red]There was an error obtaining player information.[/red]")
+    ErrorMSG(2)
+  try:
+    browref()
+  except:
+    print("[red]There was an error obtaining serverbrowser information.[/red]")
+    ErrorMSG(2)
+  try:
+    servref()
+  except:
+    print("[red]There was an error obtaining server-based information.[/red]")
+    ErrorMSG(2)
+
+def ShowCredits():
+  print("Wow lots of peoples names here")
+
+def runtime():
+  cls()
+  RefreshAll()
+  print("[orange3]Welcome to Ion Mod Manager[/orange3]")
+  print("[orange1]Please select an action: \n(1: Server Browser) (2: Coming Soon) (3: Credits)[/orange1]")
+  inputinfo = input("")
+  if inputinfo == "1":
+    print("serverbrowser")
+  if inputinfo == "2":
+    print("Coming Soon (WIP)")
+  if inputinfo == "3":
+    ShowCredits()
+
+runtime()
